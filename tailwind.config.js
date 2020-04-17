f71a6890bbc7947732756b03e3f662ad1de9c35b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-17 18:58:52 +0100

    Add Maven Pro Google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "maven" : ["Maven Pro", "sans serif"]
+      }
+    },
   },
   variants: {},
   plugins: [],
