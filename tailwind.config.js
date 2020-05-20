34924285c98fc3b8c500a0808b1eaa42a9247eea
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-21 00:38:50 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "maven" : ["Maven Pro", "sans serif"]
+        "maven" : ["Maven Pro", "sans-serif"]
       }
     },
+    colors: {
+      white: "#FFFFFF",
+      textColor: "#5B5B5B",
+      grey: "#EEEEEE",
+      secondaryColor: "#47465A",
+      purple: "#48465B"
+    }
   },
   variants: {},
   plugins: [],
