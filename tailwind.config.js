bcfb20382d1b1fe3448b032fef449c382ab03efc
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-03 19:47:14 +0100

    Design contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,23 @@ module.exports = {
     extend: {
       fontFamily: {
         "maven" : ["Maven Pro", "sans-serif"]
+      },
+      colors: {
+        white: "#FFFFFF",
+        textColor: "#5B5B5B",
+        grey: "#EEEEEE",
+        secondaryColor: "#47465A",
+        purple: "#48465B",
+        headingColor: "#353535",
+        headingColor2: "#484848"
+      },
+      spacing: {
+       '500': '31.25rem'
+      },
+      width: {
+        '4/7': '45%'
       }
     },
-    colors: {
-      white: "#FFFFFF",
-      textColor: "#5B5B5B",
-      grey: "#EEEEEE",
-      secondaryColor: "#47465A",
-      purple: "#48465B",
-      headingColor: "#353535",
-      headingColor2: "#484848"
-    }
   },
   variants: {},
   plugins: [],
